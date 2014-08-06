6f9df6c96c6ea370d893c61b1dac9f684fae7f99
malliina <malliina123@gmail.com>
2014-08-06 15:07:10 +0300

    ver

--- project/TemplateBuild.scala
+++ project/TemplateBuild.scala
@@ -10,7 +10,7 @@ object TemplateBuild extends Build {
   lazy val template = SbtUtils.testableProject("template").settings(projectSettings: _*)
 
   lazy val projectSettings = Seq(
-    scalaVersion := "2.11.1",
+    scalaVersion := "2.11.2",
     fork in Test := true
   )
 }
